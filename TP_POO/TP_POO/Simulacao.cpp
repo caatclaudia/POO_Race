f67f86c819d5a3a9735aa2683a93feb90de93a96
caatclaudia <a21270617@isec.pt>
2019-12-18 22:34:00 +0000

    Eliminados ficheiros desnecessários

--- TP_POO/TP_POO/Simulacao.cpp
+++ TP_POO/TP_POO/Simulacao.cpp
@@ -146,15 +146,16 @@ void Simulacao::addMensagem(string s)
 
 void Simulacao::addMensagemAcidente(vector<Corrida*> c)
 {
+	bool ACIDENTE = false;
 	ostringstream os;
 
 	for (auto ptr = c.begin(); ptr != c.end(); ptr++) {
-		if ((*ptr)->getCarro()->getAcidente() != CARRO_IRREPARAVEL)
-			os << "Nao houve ";
-
-		os << "Acidente do piloto " << (*ptr)->getParticipante()->getNome() << " (" << (*ptr)->getCarro()->getID() << ")" << endl;
-		addMensagem(os.str());
-		os.str("");
+		if ((*ptr)->getCarro()->getAcidente() == CARRO_IRREPARAVEL) {
+			os << "Acidente do piloto " << (*ptr)->getParticipante()->getNome() << " (" << (*ptr)->getCarro()->getID() << ")" << endl;
+			addMensagem(os.str());
+			os.str("");
+			ACIDENTE = true;
+		}
 
 		if ((*ptr)->getCarro()->getEmergencia() == EMERGENCIA_ON) {
 			os << "O piloto " << (*ptr)->getParticipante()->getNome() << " (" << (*ptr)->getCarro()->getID() << ") ativou emergencia" << endl;
@@ -162,8 +163,14 @@ void Simulacao::addMensagemAcidente(vector<Corrida*> c)
 			os.str("");
 		}
 		// FAZER a prob do piloto surpresa
-			
 	}
+	if (!ACIDENTE) 
+	{
+		os << "Sem Acidentes!" << endl;
+		addMensagem(os.str());
+		os.str("");
+	}
+
 }
 
 vector<string> Simulacao::getListaMensagens() const
